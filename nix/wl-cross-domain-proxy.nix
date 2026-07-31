69a4382a7abe6bdaf38f7f12ccb50b1f33c30661
zeroqn <zeroqn@pm.me>
2026-07-31 11:46:01 +0000

    fix(nix): use local wl-cross-domain-proxy source

--- nix/wl-cross-domain-proxy.nix
+++ nix/wl-cross-domain-proxy.nix
@@ -1,17 +1,12 @@
 { lib
 , rustPlatform
-, fetchgit
 }:
 
 rustPlatform.buildRustPackage {
   pname = "wl-cross-domain-proxy";
   version = "0.1.0";
 
-  src = fetchgit {
-    url = "https://codeberg.org/drakulix/wl-cross-domain-proxy.git";
-    rev = "c6ce1ca89fb4d6f4f18d3aaf88324d40d4589177";
-    hash = "sha256-ydyT4DFzWzhzOZR591UOgLjVQt/v6hRSNjzM3QtohlU=";
-  };
+  src = ../deps/wl-cross-domain-proxy;
 
   cargoHash = "sha256-k3dmxIuCQoOrn/VwauTdzuRw/XKQB6LPLgO5ql0rE7E=";
 
